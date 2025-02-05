6e3d1ac395d05c9cb7d6cfd7813d9622dd28dd60
Dustin Lagoy <dustin@lagoy.org>
2025-02-05 13:16:33 -0800

    basic cache locking with srtm downloads

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,7 @@
 #[macro_use]
 extern crate rocket;
 use rocket::fs::NamedFile;
+use rocket::State;
 use std::env;
 use std::path;
 #[macro_use]
@@ -9,25 +10,23 @@ mod slope;
 mod srtm;
 mod tile;
 
-#[get("/")]
-fn index() -> String {
-    let path = srtm::download(13, 3).unwrap();
-    format!("got {}", path.to_str().unwrap())
-}
-
 #[get("/<zoom>/<x>/<y_with_extension>")]
-async fn slope_tiles(zoom: u8, x: u32, y_with_extension: &str) -> Option<NamedFile> {
+async fn slope_tiles(
+    elev: &State<srtm::SRTM>,
+    zoom: u8,
+    x: u32,
+    y_with_extension: &str,
+) -> Option<NamedFile> {
     let y = y_with_extension
         .strip_suffix(".png")
         .unwrap()
         .parse::<u32>()
         .unwrap();
-    let cache = env::var("FLYTILE_CACHE_DIR").unwrap_or("/tmp".into());
     let geopoint = tile::square_to_geodetic(&tile::tile_to_square(zoom, x as f64, y as f64));
     println!("geopoint {:?}", geopoint);
+    let elevation = elev.get(geopoint).await.unwrap();
+    println!("elevation {:?}", elevation);
     let shade = rocket::tokio::task::spawn_blocking(move || {
-        let elevation = srtm::get(geopoint).unwrap();
-        println!("elevation {:?}", elevation);
         let elevation_tile = tile::single_tile(elevation, zoom, x as f64, y as f64).unwrap();
         println!("elevation tile {:?}", elevation_tile);
         let slope = slope::slope(elevation_tile).unwrap();
@@ -49,7 +48,8 @@ async fn slope_tiles(zoom: u8, x: u32, y_with_extension: &str) -> Option<NamedFi
 
 #[launch]
 fn rocket() -> _ {
+    let cache = env::var("FLYTILE_CACHE_DIR").unwrap_or("/tmp".into());
     rocket::build()
-        .mount("/", routes![index])
+        .manage(srtm::SRTM::new(path::Path::new(&cache).join("srtm")))
         .mount("/slope", routes![slope_tiles])
 }
--- src/srtm.rs
+++ src/srtm.rs
@@ -7,6 +7,7 @@ use std::path::Path;
 use std::path::PathBuf;
 use std::process;
 use std::time;
+use tokio;
 extern crate reqwest;
 
 // const URL: &'static str = "https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_{i_lon:02d}_{i_lat:02d}.zip";
@@ -25,85 +26,100 @@ extern crate reqwest;
 //                 with open(out_name, "wb") as out_file:
 //                     out_file.write(memory_file.read())
 
+pub struct SRTM {
+    cache: PathBuf,
+    download_lock: tokio::sync::Mutex<u8>,
+}
+
+impl SRTM {
+    pub fn new(cache: PathBuf) -> Self {
+        SRTM {
+            cache,
+            download_lock: tokio::sync::Mutex::new(0),
+        }
+    }
+
+    pub async fn get(&self, point: tile::GeoPoint) -> Option<PathBuf> {
+        let (i_lon, i_lat) = srtm_tile(point);
+        self.get_tile(i_lon, i_lat).await
+    }
+
+    pub async fn get_tile(&self, i_lon: i32, i_lat: i32) -> Option<PathBuf> {
+        let cache = env::var("FLYTILE_CACHE_DIR").unwrap_or("/tmp".into());
+        let output_dir = Path::new(&cache).join("srtm");
+        let output = output_dir.join(format!("srtm_{:02}_{:02}.tif", i_lon, i_lat));
+        let _lock = self.download_lock.lock().await;
+        if !output_dir.exists() {
+            fs::create_dir_all(output_dir).ok()?;
+        }
+        println!("trying srtm {:?}", output);
+        if !output.exists() {
+            self.extract(self.download(i_lon, i_lat).await?);
+        }
+        if output.exists() {
+            return Some(output);
+        }
+        None
+    }
+
+    pub async fn get_all(&self, bounds: tile::Bounds) -> Option<PathBuf> {
+        let (nw_lon, nw_lat) = srtm_tile(bounds.north_west);
+        let (ne_lon, ne_lat) = srtm_tile(bounds.north_east);
+        let (sw_lon, sw_lat) = srtm_tile(bounds.south_west);
+        let (se_lon, se_lat) = srtm_tile(bounds.south_east);
+        let min_lon = nw_lon.min(sw_lon);
+        let min_lat = sw_lat.min(se_lat);
+        let max_lon = ne_lon.max(se_lon);
+        let max_lat = nw_lat.max(ne_lat);
+        let mut files = vec![];
+        for i in min_lon..max_lon + 1 {
+            for j in min_lat..max_lat + 1 {
+                files.push(self.get_tile(i, j).await?);
+            }
+        }
+        return make_vrt(&files);
+    }
+
+    pub async fn download(&self, i_lon: i32, i_lat: i32) -> Option<PathBuf> {
+        let url = format!(
+        "https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_{i_lon:02}_{i_lat:02}.zip",
+        i_lon=i_lon,
+        i_lat=i_lat
+    );
+        let output = Path::new(&self.cache).join(format!("{}_{}.zip", i_lon, i_lat));
+        println!("downloading {}", url);
+        let client = reqwest::Client::builder()
+            .timeout(time::Duration::from_secs(180))
+            .build()
+            .ok()?;
+        let response = client.get(url).send().await.ok()?;
+        // let response = reqwest::blocking::get(url).ok()?;
+        response.error_for_status_ref().ok()?;
+        let mut content = Cursor::new(response.bytes().await.ok()?);
+        let mut file = fs::File::create(&output).ok()?;
+        io::copy(&mut content, &mut file).ok()?;
+        return Some(output.to_path_buf());
+    }
+
+    fn extract(&self, path: PathBuf) {
+        let file = fs::File::open(&path).unwrap();
+        let mut archive = zip::ZipArchive::new(file).unwrap();
+        for i in 0..archive.len() {
+            let mut file = archive.by_index(i).unwrap();
+            let outpath = Path::new(&self.cache).join(file.enclosed_name().unwrap());
+            println!("extracting {:?}", outpath);
+            let mut outfile = fs::File::create(&outpath).unwrap();
+            io::copy(&mut file, &mut outfile).unwrap();
+        }
+    }
+}
+
 pub fn srtm_tile(point: tile::GeoPoint) -> (i32, i32) {
     // lat = 60 - 5 * (index - 1)
     // lon = -180 + 5 * (index - 1)
     let i_lon: i32 = ((180.0 + point.longitude) / 5.0) as i32 + 1;
     let i_lat: i32 = ((60.0 - point.latitude) / 5.0) as i32 + 1;
     return (i_lon, i_lat);
-}
-
-pub fn retrieve_all(bounds: tile::Bounds) -> Option<PathBuf> {
-    let (nw_lon, nw_lat) = srtm_tile(bounds.north_west);
-    let (ne_lon, ne_lat) = srtm_tile(bounds.north_east);
-    let (sw_lon, sw_lat) = srtm_tile(bounds.south_west);
-    let (se_lon, se_lat) = srtm_tile(bounds.south_east);
-    let min_lon = nw_lon.min(sw_lon);
-    let min_lat = sw_lat.min(se_lat);
-    let max_lon = ne_lon.max(se_lon);
-    let max_lat = nw_lat.max(ne_lat);
-    let mut files = vec![];
-    for i in min_lon..max_lon + 1 {
-        for j in min_lat..max_lat + 1 {
-            files.push(download(i, j)?);
-        }
-    }
-    return make_vrt(&files);
-}
-
-pub fn get(point: tile::GeoPoint) -> Option<PathBuf> {
-    let (i_lon, i_lat) = srtm_tile(point);
-    let cache = env::var("FLYTILE_CACHE_DIR").unwrap_or("/tmp".into());
-    let output_dir = Path::new(&cache).join("srtm");
-    let output = output_dir.join(format!("srtm_{:02}_{:02}.tif", i_lon, i_lat));
-    if !output_dir.exists() {
-        fs::create_dir_all(output_dir).ok()?;
-    }
-    println!("trying srtm {:?}", output);
-    if !output.exists() {
-        extract(download(i_lon, i_lat)?);
-    }
-    if output.exists() {
-        return Some(output);
-    }
-    None
-}
-
-pub fn download(i_lon: i32, i_lat: i32) -> Option<PathBuf> {
-    let url = format!(
-        "https://srtm.csi.cgiar.org/wp-content/uploads/files/srtm_5x5/TIFF/srtm_{i_lon:02}_{i_lat:02}.zip",
-        i_lon=i_lon,
-        i_lat=i_lat
-    );
-    let cache = env::var("FLYTILE_CACHE_DIR").unwrap_or("/tmp".into());
-    let output = Path::new(&cache)
-        .join("srtm")
-        .join(format!("{}_{}.zip", i_lon, i_lat));
-    println!("downloading {}", url);
-    let client = reqwest::blocking::Client::builder()
-        .timeout(time::Duration::from_secs(180))
-        .build()
-        .ok()?;
-    let response = client.get(url).send().ok()?;
-    // let response = reqwest::blocking::get(url).ok()?;
-    response.error_for_status_ref().ok()?;
-    let mut content = Cursor::new(response.bytes().ok()?);
-    let mut file = fs::File::create(&output).ok()?;
-    io::copy(&mut content, &mut file).ok()?;
-    return Some(output.to_path_buf());
-}
-
-fn extract(path: PathBuf) {
-    let file = fs::File::open(&path).unwrap();
-    let mut archive = zip::ZipArchive::new(file).unwrap();
-    let parent = path.parent().unwrap();
-    for i in 0..archive.len() {
-        let mut file = archive.by_index(i).unwrap();
-        let outpath = Path::new(&parent).join(file.enclosed_name().unwrap());
-        println!("extracting {:?}", outpath);
-        let mut outfile = fs::File::create(&outpath).unwrap();
-        io::copy(&mut file, &mut outfile).unwrap();
-    }
 }
 
 fn make_vrt(paths: &Vec<PathBuf>) -> Option<PathBuf> {
@@ -130,9 +146,10 @@ mod tests {
             longitude: -120.0,
             latitude: 50.0,
         });
-        let path = download(i_lat, i_lon).unwrap();
+        let srtm = SRTM::new(Path::new("/tmp").into());
+        let path = srtm.download(i_lat, i_lon).unwrap();
         assert_eq!(path.to_str().unwrap(), "/tmp/13_3.zip");
-        extract(path);
+        srtm.extract(path);
         assert!(Path::new("/tmp/srtm_13_03.hdr").exists());
     }
 }

5af7c9804af4d1c546321685325697041f743bef
Dustin Lagoy <dustin@lagoy.org>
2025-03-07 21:15:49 -0800

    cleanup cache

--- src/cache.rs
+++ src/cache.rs
@@ -20,14 +20,6 @@ struct GeneratorError;
 type SendBack = mpsc::Sender<StdResult<PathBuf, GeneratorError>>;
 type GetBack = mpsc::Receiver<StdResult<PathBuf, GeneratorError>>;
 
-// type Generator = FnOnce() -> StdResult<PathBuf, GeneratorError> + Send + 'static;
-
-// #[derive(Debug)]
-// pub struct GeneratingRequest {
-//     request: Request,
-//     generator: Generator,
-// }
-
 #[derive(Debug)]
 pub struct Request {
     key: PathBuf,
@@ -38,14 +30,10 @@ pub struct Request {
 pub struct Cache {
     pub cache: PathBuf,
     items: collections::HashMap<PathBuf, PathBuf>,
-    max_size_bytes: usize,
+    _max_size_bytes: usize,
     item_timeout: time::Duration,
-    in_progress: collections::HashMap<
-        PathBuf,
-        // time::Instant,
-        thread::JoinHandle<StdResult<PathBuf, GeneratorError>>,
-    >,
-    progress_lock: std::sync::Mutex<u8>,
+    in_progress:
+        collections::HashMap<PathBuf, thread::JoinHandle<StdResult<PathBuf, GeneratorError>>>,
     to_return: collections::HashMap<PathBuf, Vec<SendBack>>,
 }
 
@@ -56,10 +44,9 @@ impl Cache {
         Ok(Cache {
             cache: path,
             items,
-            max_size_bytes: 10_000_000_000,
+            _max_size_bytes: 10_000_000_000,
             item_timeout: time::Duration::from_secs(86400),
             in_progress: collections::HashMap::new(),
-            progress_lock: std::sync::Mutex::new(0),
             to_return: collections::HashMap::new(),
         })
     }
@@ -110,98 +97,6 @@ impl Cache {
             }
         }
     }
-
-    // pub fn get_or_set<F>(&mut self, key: &PathBuf, generator: F) -> Result<PathBuf>
-    // where
-    //     F: FnOnce() -> Result<PathBuf> + Send + 'static,
-    // {
-    //     let (tx, rx) = mpsc::channel();
-    //     let mut generating_item = false;
-    //     {
-    //         let _guard = self.progress_lock.lock().unwrap();
-    //         if let Some(path) = self.items.get(key) {
-    //             return Ok(path.clone());
-    //         }
-    //         // item not in cache
-    //         if let Some(_instant) = self.in_progress.get(key) {
-    //             // someone currently generating item
-    //             // TODO: we must release lock and then join
-    //         } else {
-    //             // no one currently generating item, generate it now
-    //             generating_item = true;
-    //             thread::spawn(move || tx.send(generator()));
-    //             self.in_progress
-    //                 .insert(key.to_path_buf(), time::Instant::now());
-    //         }
-    //     }
-    //     // match self.in_progress.get(key).unwrap().join() {
-    //     if generating_item {
-    //         // wait for the item to be generated and add it to the cache
-    //         match rx.recv().unwrap() {
-    //             Ok(item) => {
-    //                 let _guard = self.progress_lock.lock().unwrap();
-    //                 self.items.insert(key.clone(), item);
-    //                 self.in_progress.remove(key);
-    //                 if let Some(path) = self.items.get(key) {
-    //                     return Ok(path.clone());
-    //                 }
-    //                 Err(anyhow!("oops"))
-    //             }
-    //             Err(x) => {
-    //                 let _guard = self.progress_lock.lock().unwrap();
-    //                 self.in_progress.remove(key);
-    //                 Err(anyhow!("{:?}", x))
-    //             }
-    //         }
-    //     } else {
-    //         // someone should be generating the item, so wait until it is complete
-    //         loop {
-    //             if let Some(path) = self.items.get(key) {
-    //                 return Ok(path.clone());
-    //             }
-    //             thread::sleep(time::Duration::from_millis(5));
-    //         }
-    //     }
-    // }
-
-    // pub fn get_or_set<F>(&mut self, key: &PathBuf, generator: F) -> Result<PathBuf>
-    // where
-    //     F: FnOnce() -> Result<PathBuf>,
-    // {
-    //     {
-    //         let _guard = self.progress_lock.lock().unwrap();
-    //         if let Some(path) = self.items.get(key) {
-    //             return Ok(path.clone());
-    //         }
-    //         // item not in cache
-    //         if let Some(instant) = self.in_progress.get(key) {
-    //             if instant.elapsed() < self.progress_timeout {
-    //                 // someone currently generating item
-    //                 // wait?
-    //             }
-    //         }
-    //         // no one currently generating item
-    //         // or previous generation timed out
-    //         self.in_progress
-    //             .insert(key.to_path_buf(), time::Instant::now());
-    //     }
-    //     match generator() {
-    //         Ok(item) => {
-    //             let _guard = self.progress_lock.lock().unwrap();
-    //             self.items.insert(key.clone(), item);
-    //             self.in_progress.remove(key);
-    //             if let Some(path) = self.items.get(key) {
-    //                 return Ok(path.clone());
-    //             }
-    //             Err(anyhow!("oops"))
-    //         }
-    //         Err(x) => {
-    //             let _guard = self.progress_lock.lock().unwrap();
-    //             self.in_progress.remove(key);
-    //             Err(x)
-    //         }
-    //     }
-    // }
 
     fn is_expired(&self, full_path: PathBuf) -> bool {
         let metadata = match fs::metadata(&full_path) {
@@ -266,6 +161,26 @@ mod tests {
         }
     }
 
+    fn insert_a(cache: &mut Cache, filename_override: &str) -> GetBack {
+        let (tx, rx) = mpsc::channel();
+        let cache_dir = cache.cache.clone();
+        let filename = filename_override.to_string();
+        let generator = move || {
+            thread::sleep(time::Duration::from_millis(100));
+            let full_path = cache_dir.join(&filename);
+            fs::write(&full_path, "a").unwrap();
+            return Ok(full_path);
+        };
+        cache.try_get(
+            Request {
+                key: "a".into(),
+                send_back: tx,
+            },
+            generator,
+        );
+        return rx;
+    }
+
     #[test]
     fn test_load_dir() {
         let dir = tempfile::tempdir().unwrap();
@@ -290,13 +205,6 @@ mod tests {
         assert!(cache.get(&"a".into()).is_none());
 
         let cache_dir = cache.cache.clone();
-        // let result = cache
-        //     .get_or_set(&"a".into(), move || {
-        //         let full_path = cache_dir.join("a");
-        //         fs::write(&full_path, "a").unwrap();
-        //         return Ok(full_path);
-        //     })
-        //     .unwrap();
         let generator = move || {
             let full_path = cache_dir.join("a");
             fs::write(&full_path, "a").unwrap();
@@ -321,48 +229,31 @@ mod tests {
     fn test_wait_for_cache_add() {
         let dir = tempfile::tempdir().unwrap();
         let mut cache = Cache::from_existing_directory(dir.path().to_path_buf()).unwrap();
-        let (tx, rx) = mpsc::channel();
-        let cache_dir = cache.cache.clone();
-        let generator = move || {
-            thread::sleep(time::Duration::from_millis(100));
-            let full_path = cache_dir.join("a");
-            fs::write(&full_path, "a").unwrap();
-            return Ok(full_path);
-        };
-        cache.try_get(
-            Request {
-                key: "a".into(),
-                send_back: tx,
-            },
-            generator,
-        );
+        let rx = insert_a(&mut cache, "a");
         assert!(rx.try_recv().is_err());
         let result = check_until(&mut cache, rx).unwrap();
         assert!(result.ends_with("a"));
         assert!(cache.cache.join("a").exists());
     }
 
-    // #[test]
-    // fn test_wait_for_cache_add() {
-    //     let dir = tempfile::tempdir().unwrap();
-    //     let mut cache = Arc::new(Cache::from_existing_directory(dir.path().to_path_buf()).unwrap());
-    //     let a_cache = cache.clone();
-    //     thread::spawn(move || {
-    //         let cache_dir = a_cache.cache.clone();
-    //         a_cache
-    //             .get_or_set(&"a".into(), move || {
-    //                 thread::sleep(time::Duration::from_secs(1));
-    //                 let full_path = cache_dir.join("a");
-    //                 fs::write(&full_path, "a").unwrap();
-    //                 return Ok(full_path);
-    //             })
-    //             .unwrap()
-    //     });
-    //     assert!(cache.get(&"a".into()).is_none());
-    //     // assert_eq!(
-    //     //     cache.get("a".into()),
-    //     //     MaybePath::NotAvailable(dir.path().to_path_buf())
-    //     // );
-    //     // assert_eq!(cache.get("a".into()), MaybePath::InProgress);
-    // }
+    #[test]
+    fn test_run_generator_only_once() {
+        let dir = tempfile::tempdir().unwrap();
+        let mut cache = Cache::from_existing_directory(dir.path().to_path_buf()).unwrap();
+        let rx_a = insert_a(&mut cache, "x");
+        let rx_b = insert_a(&mut cache, "y");
+        let rx_c = insert_a(&mut cache, "z");
+        // all the cache calls should return the result of the first generator
+        let result_a = check_until(&mut cache, rx_a).unwrap();
+        assert!(result_a.ends_with("x"));
+        let result_b = rx_b.recv().unwrap().unwrap();
+        assert!(result_b.ends_with("x"));
+        let result_c = rx_c.recv().unwrap().unwrap();
+        assert!(result_c.ends_with("x"));
+
+        assert!(cache.cache.join("x").exists());
+        // the other generators should never generate their files
+        assert!(!cache.cache.join("y").exists());
+        assert!(!cache.cache.join("z").exists());
+    }
 }
